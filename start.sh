0284d6c2daef905aae7961b601102487f0632a43
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-09-28 15:35:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update
-sudo apt install screen -y
+sudo apt install screen htop -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
