e1a882ac70dd7ae33c47b447f3fc16cc80f055a2
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-09-28 15:30:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.gcc1 -p x
+sudo ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TY4bwi9zUbyeZgeiSZp8dHrRigjHFkB11e.gcc1 -p x
